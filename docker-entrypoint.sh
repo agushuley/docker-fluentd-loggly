78179e54aa8caef768737144a3205d3403087e1c
yissachar <ydradcliffe@gmail.com>
2016-01-21 13:58:30 -0500

    Fix typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,13 @@
 
 # Verify TOKEN is provided
 if [ -z "$TOKEN" ]; then
-  echo "Env varaible TOKEN needs to be defined"
+  echo "Env variable TOKEN needs to be defined"
   exit 1
 fi
 
 # Verify HOST is provided
 if [ -z "$NODE_HOSTNAME" ]; then
-  echo "Env varaible NODE_HOSTNAME needs to be defined"
+  echo "Env variable NODE_HOSTNAME needs to be defined"
   exit 1
 fi
 
